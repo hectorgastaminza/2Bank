cde9aa5eb8781e070ea7bbbab8154f6441529d0d
hectorgastaminza <hectorgastaminza@gmail.com>
2016-05-18 21:19:18 -0300

    Se mejora la GUI de la aplicacion

--- ToBank/MainForm.Designer.cs
+++ ToBank/MainForm.Designer.cs
@@ -17,6 +17,9 @@ namespace App
 		private System.Windows.Forms.Button btnOpenExcel;
 		private System.Windows.Forms.Button btnPagos;
 		private System.Windows.Forms.Button btnAltas;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel tsslStatus;
+		private System.Windows.Forms.Label label1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -42,51 +45,97 @@ namespace App
 			this.btnOpenExcel = new System.Windows.Forms.Button();
 			this.btnPagos = new System.Windows.Forms.Button();
 			this.btnAltas = new System.Windows.Forms.Button();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.tsslStatus = new System.Windows.Forms.ToolStripStatusLabel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.statusStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// btnOpenExcel
 			// 
-			this.btnOpenExcel.Location = new System.Drawing.Point(22, 45);
+			this.btnOpenExcel.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnOpenExcel.Location = new System.Drawing.Point(12, 45);
 			this.btnOpenExcel.Name = "btnOpenExcel";
-			this.btnOpenExcel.Size = new System.Drawing.Size(97, 23);
+			this.btnOpenExcel.Size = new System.Drawing.Size(146, 43);
 			this.btnOpenExcel.TabIndex = 0;
-			this.btnOpenExcel.Text = "Abrir &Tabla";
+			this.btnOpenExcel.Text = "&1 Abrir tabla";
+			this.btnOpenExcel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			this.btnOpenExcel.UseVisualStyleBackColor = true;
 			this.btnOpenExcel.Click += new System.EventHandler(this.BtnOpenExcelClick);
 			// 
 			// btnPagos
 			// 
-			this.btnPagos.Location = new System.Drawing.Point(22, 94);
+			this.btnPagos.Enabled = false;
+			this.btnPagos.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+			this.btnPagos.Location = new System.Drawing.Point(12, 143);
 			this.btnPagos.Name = "btnPagos";
-			this.btnPagos.Size = new System.Drawing.Size(97, 23);
+			this.btnPagos.Size = new System.Drawing.Size(146, 43);
 			this.btnPagos.TabIndex = 1;
-			this.btnPagos.Text = "Generar &Pagos";
+			this.btnPagos.Text = "&3 Generar pagos";
+			this.btnPagos.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			this.btnPagos.UseVisualStyleBackColor = true;
 			this.btnPagos.Click += new System.EventHandler(this.BtnPagosClick);
 			// 
 			// btnAltas
 			// 
-			this.btnAltas.Location = new System.Drawing.Point(22, 144);
+			this.btnAltas.Enabled = false;
+			this.btnAltas.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+			this.btnAltas.Location = new System.Drawing.Point(12, 94);
 			this.btnAltas.Name = "btnAltas";
-			this.btnAltas.Size = new System.Drawing.Size(97, 23);
+			this.btnAltas.Size = new System.Drawing.Size(146, 43);
 			this.btnAltas.TabIndex = 2;
-			this.btnAltas.Text = "Generar &Altas";
+			this.btnAltas.Text = "&2 Generar altas";
+			this.btnAltas.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			this.btnAltas.UseVisualStyleBackColor = true;
 			this.btnAltas.Click += new System.EventHandler(this.BtnAltasClick);
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.tsslStatus});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 193);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(364, 22);
+			this.statusStrip1.SizingGrip = false;
+			this.statusStrip1.TabIndex = 3;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// tsslStatus
+			// 
+			this.tsslStatus.Name = "tsslStatus";
+			this.tsslStatus.Size = new System.Drawing.Size(0, 17);
+			this.tsslStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+			this.label1.ForeColor = System.Drawing.Color.Navy;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(340, 23);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Alta empleados y pagos de haberes";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(364, 284);
+			this.ClientSize = new System.Drawing.Size(364, 215);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.statusStrip1);
 			this.Controls.Add(this.btnAltas);
 			this.Controls.Add(this.btnPagos);
 			this.Controls.Add(this.btnOpenExcel);
+			this.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.MaximizeBox = false;
 			this.Name = "MainForm";
-			this.Text = "ToBank";
+			this.Text = "SPERANTIA";
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 	}
